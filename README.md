36e138d31ba5bef0bd573872bf07d74af52ff71c
privera <privera@celerity.com>
2020-07-21 20:22:29 -0400

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # programming101-ep6
 programming 101 ep6 from PyATL twitch stream at https://twitch.tv/pyatl
+
+## Intro to tools
+
+Python is a tool. It is used to make the computer do what we wsnt it to do.
+Python allows us to treat the computer as a puppet.
---
+++ basics.py
@@ -0,0 +1,67 @@
+# 1. A way to store data
+# 2. A way to loop over data
+# 3. A way to make decisions
+# 4. Display data
+
+
+# Ways to store data in python
+
+# variables
+
+# example_variable = 'example'
+
+# example_list = [1,2,3,4,5]
+
+# example_dictionary = {'1': 1, '2': 2}
+
+# example_tuple = ('a', 'b', 'b')
+
+
+# Ways to loop over data
+
+# A for loops will automatically stop when the work is done,
+
+
+
+# for each_number in example_list:
+#     print(each_number)
+
+# for key, value in example_dictionary.items():
+#     print('key', key, 'value', value)
+
+
+# list_of_users = ['john', 'mary', 'jane', 'cyndi']
+
+
+# for user in list_of_users:
+#     print(user.capitalize())
+
+
+
+# While loops
+# While loops do something as long as a condition is met.
+# its a for loop that needs to make a decision.
+
+#list_of_users = ['john', 'mary', 'jane', 'cyndi']
+#                  0       1        2       3
+
+
+# while list_of_users:
+#     print(list_of_users[0])
+#     list_of_users.pop(count)
+
+
+# count = 0
+
+# only loop while count is less or equal to the length of the list
+# while count <= (len(list_of_users) - 1):
+    # len neasures the length of the list. Which is 4.
+    # if we look at the list, there are 4 items.
+    # but the fourth item has an idex of 3.
+    # thats because the index starts at 0.
+    # print(f'Count is {count}', list_of_users[count])
+    # count += 1
+
+
+
+    
